26433b6e988d02095917dfce580b1afaa9354954
wildrimak <wildrimak@gmail.com>
2020-10-09 20:25:33 -0300

    Entidade Autor adicionada com validações de entrada na api e no banco de dados

---
+++ src/main/java/br/com/zup/casadocodigo/domain/models/Autor.java
@@ -0,0 +1,82 @@
+package br.com.zup.casadocodigo.domain.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Autor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotEmpty
+	@Column(nullable = false)
+	private String nome;
+
+	@NotEmpty
+	@Email
+	@Column(nullable = false)
+	private String email;
+
+	@NotEmpty
+	@Size(max = 400)
+	@Column(nullable = false)
+	private String descricao;
+
+	private Date dataCadastro;
+
+	@Deprecated
+	public Autor() {
+		this.dataCadastro = new Date();
+	}
+
+	public Autor(@NotEmpty String nome, @NotEmpty @Email String email, @NotEmpty @Size(max = 400) String descricao) {
+
+		this();
+		this.nome = nome;
+		this.email = email;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+}
